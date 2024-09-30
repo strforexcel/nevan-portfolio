b9a0c0838fbe95c424cde29be9833bba91c2cc1c
strforexcel <117307203+strforexcel@users.noreply.github.com>
2024-09-30 18:48:01 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import portfolioItems from "./data";
 
 function App() {
+  // State for the selected category filter
+  const [selectedCategory, setSelectedCategory] = useState("Website");
+
+  // Filtered portfolio items based on the selected category
+  const filteredItems = portfolioItems.filter((item) => {
+    return item.category === selectedCategory; // Show filtered items
+  });
+
+  // Sub-menu options
+  const categories = ["Website", "UI/UX"];
+  const capabilities = [
+    "Front End Development",
+    "UI/UX Designer",
+    "Web Design",
+    "Figma",
+    "Photoshop",
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="layout">
+        <div className="gradient-top"></div>
+        <div className="main-title">
+          <div className="name-title">Hello,</div>
+          <div className="name-title">I'm Nevan Marshelim</div>
+          <div className="title-desc">
+            Computer Science Graduate from BINUS University
+          </div>
+          <div className="title-desc">
+            Formerly specializing in Interactive Multimedia
+          </div>
+        </div>
+        <div className="works">
+          <div className="title">Works</div>
+          <div className="submenu">
+            {categories.map((category, index) => (
+              <span
+                key={index}
+                className={`submenu-item ${
+                  selectedCategory === category ? "active" : ""
+                }`}
+                onClick={() => setSelectedCategory(category)}
+              >
+                {category}
+              </span>
+            ))}
+          </div>
+          {/* Display filtered portfolio items */}
+          <div className="portfolio-grid">
+            {filteredItems.map((item) => (
+              <a
+                className="link"
+                href={item.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div key={item.id} className="portfolio-item">
+                  <img src={item.image} alt={item.title} />
+                  <div className="item-title">{item.title}</div>
+                  <div className="item-desc">{item.desc}</div>
+                </div>
+              </a>
+            ))}
+          </div>
+        </div>
+        <div className="about-n-capabilities">
+          <div className="about-container">
+            <div className="title">About me</div>
+            <div className="about-desc">
+              Hello, I'm Nevan Marshelim, a passionate Computer Science graduate
+              specializing in Multimedia. With two years of experience in a
+              student organization that honed my teamwork and communication
+              skills, I also bring one year of practical experience in ReactJS
+              as an intern. I thrive in collaborative environments and am eager
+              to apply my skills in innovative projects.
+            </div>
+          </div>
+          <div className="capabilities-container">
+            <div className="title">My Capabilities</div>
+            <ul className="capabilities-list">
+              {capabilities.map((capability, index) => (
+                <li key={index} className="capability-item">
+                  {capability}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="gradient-bottom"></div>
+      </div>
+    </>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,161 @@
+import hospital from "./img/1. hospital search.jpg";
+import diet from "./img/2. diet tracker.jpg";
+import homework from "./img/3. homework time.jpg";
+import movielist from "./img/4. movielist.jpg";
+import doto from "./img/5. doto 2.jpg";
+import ourtrip from "./img/6. ourtrip.jpg";
+import calm from "./img/7. calm.jpg";
+import allgadget from "./img/8. allgadget.jpg";
+import hayoke from "./img/9. hayoke-web.jpg";
+
+// const portfolioItems = [
+//   {
+//     id: 1,
+//     title: "Hospital Search",
+//     category: "UI/UX",
+//     link: "https://www.figma.com/proto/wgp6teVZPptZqn5fjwHFhu/",
+//     image: hospital,
+//     desc: "A design to search nearby hospital based on users location and needs",
+//   },
+//   {
+//     id: 2,
+//     title: "Diet Tracker",
+//     category: "UI/UX",
+//     link: "https://www.figma.com/proto/D2zCv41bC10W5Hm6bNIXCa",
+//     image: diet,
+//     desc: "A design to track users diet also has other function such as calorie calculator, recommended sport activity and healthy recipe to support the diet",
+//   },
+//   {
+//     id: 3,
+//     title: "HomeWork Time!",
+//     category: "UI/UX",
+//     link: "https://www.figma.com/proto/4OgARrkGeJtbYHGuHC0Nyc/",
+//     image: homework,
+//     desc: "A design that help users remember their homework assignments",
+//   },
+//   {
+//     id: 4,
+//     title: "Hayoke Global Trade",
+//     category: "Website",
+//     link: "https://hayokeglobaltrade.com/",
+//     image: hayoke,
+//     desc: "A website that shows the Hayoke company profile",
+//   },
+//   {
+//     id: 5,
+//     title: "MovieList",
+//     category: "Website",
+//     link: "https://github.com/strforexcel/MovieList",
+//     image: movielist,
+//     desc: "Front-end design for movie database website",
+//   },
+//   {
+//     id: 6,
+//     title: "DOTO 2",
+//     category: "Website",
+//     link: "https://github.com/strforexcel/Doto-2",
+//     image: doto,
+//     desc: "Front-end design for gaming information website",
+//   },
+//   {
+//     id: 7,
+//     title: "OurTrip",
+//     category: "Website",
+//     link: "https://github.com/strforexcel/OurTrip",
+//     image: ourtrip,
+//     desc: "Front-end and Back-end design",
+//   },
+//   {
+//     id: 8,
+//     title: "Calm App",
+//     category: "UI/UX",
+//     link: "https://www.figma.com/proto/mbOytBvHVBByA4F4XjxvP4/Calm-App?node-id=33-678&node-type=canvas&t=jqgITQJRhovCtqUm-1&scaling=min-zoom&content-scaling=fixed&page-id=27%3A608&starting-point-node-id=27%3A609",
+//     image: calm,
+//     desc: "A design for an app that educate users about their mental health and users can consult with professional through this app",
+//   },
+//   {
+//     id: 9,
+//     title: "All Gadget",
+//     category: "UI/UX",
+//     link: "https://www.figma.com/proto/iQrqDExYxkWHwTSPvCaujq/",
+//     image: allgadget,
+//     desc: "A design for gadget seller platform",
+//   },
+// ];
+
+const portfolioItems = [
+  {
+    id: 1,
+    title: "Hospital Search",
+    category: "UI/UX",
+    link: "https://www.figma.com/proto/wgp6teVZPptZqn5fjwHFhu/",
+    image: hospital,
+    desc: "A user-friendly design that allows individuals to find nearby hospitals tailored to their location and specific needs.",
+  },
+  {
+    id: 2,
+    title: "Diet Tracker",
+    category: "UI/UX",
+    link: "https://www.figma.com/proto/D2zCv41bC10W5Hm6bNIXCa",
+    image: diet,
+    desc: "An intuitive design for tracking dietary habits, featuring a calorie calculator, exercise recommendations, and healthy recipes to support weight management.",
+  },
+  {
+    id: 3,
+    title: "HomeWork Time!",
+    category: "UI/UX",
+    link: "https://www.figma.com/proto/4OgARrkGeJtbYHGuHC0Nyc/",
+    image: homework,
+    desc: "A helpful design that assists users in remembering their homework assignments and deadlines.",
+  },
+  {
+    id: 4,
+    title: "Hayoke Global Trade",
+    category: "Website",
+    link: "https://hayokeglobaltrade.com/",
+    image: hayoke,
+    desc: "An informative website showcasing the profile and services of Hayoke Global Trade.",
+  },
+  {
+    id: 5,
+    title: "MovieList",
+    category: "Website",
+    link: "https://github.com/strforexcel/MovieList",
+    image: movielist,
+    desc: "A front-end design for a comprehensive movie database website, offering users easy navigation and information access.",
+  },
+  {
+    id: 6,
+    title: "DOTO 2",
+    category: "Website",
+    link: "https://github.com/strforexcel/Doto-2",
+    image: doto,
+    desc: "A front-end design for a gaming information website that provides insights and resources for DOTO 2 players.",
+  },
+  {
+    id: 7,
+    title: "OurTrip",
+    category: "Website",
+    link: "https://github.com/strforexcel/OurTrip",
+    image: ourtrip,
+    desc: "A complete design encompassing both front-end and back-end for a travel planning platform.",
+  },
+  {
+    id: 8,
+    title: "Calm App",
+    category: "UI/UX",
+    link: "https://www.figma.com/proto/mbOytBvHVBByA4F4XjxvP4/Calm-App?node-id=33-678&node-type=canvas&t=jqgITQJRhovCtqUm-1&scaling=min-zoom&content-scaling=fixed&page-id=27%3A608&starting-point-node-id=27%3A609",
+    image: calm,
+    desc: "A design for an app focused on educating users about mental health, offering consultations with professionals within the platform.",
+  },
+  {
+    id: 9,
+    title: "All Gadget",
+    category: "UI/UX",
+    link: "https://www.figma.com/proto/iQrqDExYxkWHwTSPvCaujq/",
+    image: allgadget,
+    desc: "A sleek design for a platform dedicated to gadget sales, providing an engaging shopping experience.",
+  },
+];
+
+export default portfolioItems;
